ccbe5ddc5a4dae2cf15b8a8b2e128319d3e276fe
Noelle-1220 <m.ingabire3@alustudent.com>
2026-08-02 23:28:54 +0400

    Task well done

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const films = JSON.parse(body).results;
+  const count = films.filter((film) => film.characters.some(
+    (character) => character.endsWith('/18/')
+  )).length;
+  console.log(count);
+});
